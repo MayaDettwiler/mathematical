c949aa62e78b8b958cb3c4d93a4f61436806b0c6
MayaDettwiler <73507307+MayaDettwiler@users.noreply.github.com>
2021-01-06 10:11:13 -0600

    Add files via upload

---
+++ mathOutline.py
@@ -0,0 +1,66 @@
+'''
+This outline will help solidify concepts from the Mathematical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables and use the + operator to combine them into
+#one new variable.
+x = "maya" + "dettwiler"
+print(x)
+
+#2) Make two int variables and use the + operator to combine them into
+#one new variable.
+y = 4 + 4
+print(y)
+
+#3) Make two int variables and use the - operator to combine them into
+#one new variable.
+z = 10 - 5
+print(z)
+
+
+#4) Make two float variables  and use the - operator to combine them into
+#one new variable.
+a = 2.5 - 1.5
+print(a)
+
+#5) Make two int variables and use the * operator to combine them into
+#one new variable.
+b = 5 * 5
+print(b)
+
+#6) Make one string variable and one int variable and use the * operator 
+#to combine them into one new variable.
+c = "yes" * 7
+print(c)
+
+#7) Make two int variables and use the / operator to combine them into
+#one new variable.
+d = 6 / 3
+print(d)
+
+#8) Make two float variables and use the / operator to combine them into
+#one new variable.
+e = 6.5 / 2.5 
+print(e)
+
+#9) Make two int variables and use the % operator to combine them into
+#one new variable.
+f = 5 % 4
+print(f)
+
+#10) Make two int variables and use the // operator to combine them into
+#one new variable.
+g = 2 // 2
+print(g)
+
+#11) Make two int variables and use the ** operator to combine them into
+#one new variable.
+h = 4 ** 4 
+print(h)
+
+
+#12) Make any type of operation with a common SYNTAX error
+l = 3 + 3 
+prit(l)
+
